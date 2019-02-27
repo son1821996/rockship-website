67140ecf9541497a9a9a5b92349f58c0c408f5a3
Hung Tran <hungkimtran16102000@gmail.com>
2019-02-27 11:19:01 +0700

    career page half down

--- src/components/Career/OpenPositionSection.jsx
+++ src/components/Career/OpenPositionSection.jsx
@@ -5,10 +5,57 @@ import '../../stylesheets/Career/OpenPositionSection.scss';
 class OpenPositionSection extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      iosActivated: true,
+      androidActivated: false,
+      frontEndActivated: false,
+      backEndActivated: false,
+    };
+  }
+
+  activateIos = () => {
+    this.setState({
+      iosActivated: true,
+      androidActivated: false,
+      frontEndActivated: false,
+      backEndActivated: false,
+    });
+  }
+
+  activateAndroid = () => {
+    this.setState({
+      iosActivated: false,
+      androidActivated: true,
+      frontEndActivated: false,
+      backEndActivated: false,
+    });
+  }
+
+  activateFrontEnd = () => {
+    this.setState({
+      iosActivated: false,
+      androidActivated: false,
+      frontEndActivated: true,
+      backEndActivated: false,
+    });
+  }
+
+  activateBackEnd = () => {
+    this.setState({
+      iosActivated: false,
+      androidActivated: false,
+      frontEndActivated: false,
+      backEndActivated: true,
+    });
   }
 
   render() {
+    const {
+      iosActivated,
+      androidActivated,
+      frontEndActivated,
+      backEndActivated,
+    } = this.state;
     return (
       <div className="open-position-section">
         <div className="position-at-rockship">
@@ -32,16 +79,96 @@ class OpenPositionSection extends Component {
             <div className="open-position-group">
               <div className="position-group">
                 <div className="position ios">
-                  <div />
+                  <div className="position-clicking-area">
+                    <img
+                      alt="why-choose-copy"
+                      src="./img/why-choose-copy.png"
+                      srcSet="./img/why-choose-copy@2x.png 2x, ./img/why-choose-copy@3x.png 3x,"
+                      className="position-icon"
+                      onClick={this.activateIos}
+                      style={{
+                        opacity: `${iosActivated ? '1' : '0.1'}`,
+                      }}
+                    />
+                  </div>
+                  <div
+                    className="title"
+                    style={{
+                      opacity: `${iosActivated ? '1' : '0.1'}`,
+                    }}
+                  >
+                    IOS
+                  </div>
                 </div>
                 <div className="position android">
-                  <div />
+                  <div className="position-clicking-area">
+                    <img
+                      alt="why-choose-copy"
+                      src="./img/android.png"
+                      srcSet="./img/android@2x.png 2x, ./img/android@3x.png 3x,"
+                      className="position-icon"
+                      onClick={this.activateAndroid}
+                      style={{
+                        opacity: `${androidActivated ? '1' : '0.1'}`,
+                      }}
+                    />
+                  </div>
+                  <div
+                    className="title"
+                    style={{
+                      opacity: `${androidActivated ? '1' : '0.1'}`,
+                    }}
+                  >
+                    Android
+                  </div>
                 </div>
-                <div className="position frontend">
-                  <div />
+                <div
+                  className="position frontend"
+                >
+                  <div className="position-clicking-area">
+                    <img
+                      alt="why-choose-copy"
+                      src="./img/frontend.png"
+                      srcSet="./img/frontend@2x.png 2x, ./img/frontend@3x.png 3x,"
+                      className="position-icon"
+                      onClick={this.activateFrontEnd}
+                      style={{
+                        opacity: `${frontEndActivated ? '1' : '0.1'}`,
+                      }}
+                    />
+                  </div>
+                  <div
+                    className="title"
+                    style={{
+                      opacity: `${frontEndActivated ? '1' : '0.1'}`,
+                    }}
+                  >
+                    Frontend
+                  </div>
                 </div>
-                <div className="position backend">
-                  <div />
+                <div
+                  className="position backend"
+                >
+                  <div className="position-clicking-area">
+                    <img
+                      alt="why-choose-copy"
+                      src="./img/backend.png"
+                      srcSet="./img/backend@2x.png 2x, ./img/backend@3x.png 3x,"
+                      className="position-icon"
+                      onClick={this.activateBackEnd}
+                      style={{
+                        opacity: `${backEndActivated ? '1' : '0.1'}`,
+                      }}
+                    />
+                  </div>
+                  <div
+                    className="title"
+                    style={{
+                      opacity: `${backEndActivated ? '1' : '0.1'}`,
+                    }}
+                  >
+                    Backend
+                  </div>
                 </div>
               </div>
             </div>
